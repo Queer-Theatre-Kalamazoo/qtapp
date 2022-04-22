4701b86416675f5e2a1078ddf7fb72a8f4eefc67
Hazel <hazel@queertk.org>
2022-04-22 11:12:41 -0400

    Migrate responsive template from idlegame, more styling

---
+++ queertk/static/js/base.js
@@ -0,0 +1,26 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+// We need to keep track of faded in elements so we can apply fade out later in CSS
+document.addEventListener('animationstart', function (e) {
+    if (e.animationName === 'fade-in') {
+        e.target.classList.add('did-fade-in');
+    }
+});
+
+document.addEventListener('animationend', function (e) {
+    if (e.animationName === 'fade-out') {
+        e.target.classList.remove('did-fade-in');
+    }
+});
