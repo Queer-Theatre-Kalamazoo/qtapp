ff34e4f599dc389e754fd7fc14627d95d8b58d95
Hazel <hazel@queertk.org>
2022-04-22 18:30:51 -0400

    Dynamic titles and favicons, nav ph

--- queertk/static/js/base.js
+++ queertk/static/js/base.js
@@ -1,26 +1,18 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
 }
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
-}
-
-// We need to keep track of faded in elements so we can apply fade out later in CSS
-document.addEventListener('animationstart', function (e) {
-    if (e.animationName === 'fade-in') {
-        e.target.classList.add('did-fade-in');
-    }
-});
-
-document.addEventListener('animationend', function (e) {
-    if (e.animationName === 'fade-out') {
-        e.target.classList.remove('did-fade-in');
-    }
-});
