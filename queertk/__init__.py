789ece0c5f7ffec0201414f321705a5b282b55ec
Hazel <hazel@queertk.org>
2022-04-23 15:36:41 -0400

    avatar to header, add poster to prod pages, format perf datetime

--- queertk/__init__.py
+++ queertk/__init__.py
@@ -15,14 +15,20 @@ from queertk.production.production import bp_productions
 from queertk.authentication.authentication import bp_authentication
 
 # Register blueprints
-app.register_blueprint(bp_admin)
-app.register_blueprint(bp_productions)
-app.register_blueprint(bp_authentication)
+app.register_blueprint(bp_admin, url_prefix = '/admin')
+app.register_blueprint(bp_productions, url_prefix = '/prod')
+app.register_blueprint(bp_authentication, url_prefix = '/auth')
 
 # Database setup
 db_conn_string = app.config["SQLALCHEMY_DATABASE_URI"] # Pull URI from config
 engine = create_engine(db_conn_string) # Instantiate SQLAlchemy create_engine
 
-
 import queertk.views
 import queertk.models
+
+from datetime import datetime
+
+@app.template_filter()
+def format_datetime(value):
+    
+    return value.strftime("%d %B, %Y")
--- queertk/production/production.py
+++ queertk/production/production.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 from sqlalchemy import or_
 from flask_login import current_user, login_required
 
@@ -9,7 +9,6 @@ from queertk.models import Production, ProductionNotice, Notice, NoticeType, Sea
 from database import db
 
 @bp_productions.route("/<int:prod_id>/<string:slug>")
-@login_required
 def display_production(prod_id, slug):
 
     # TODO A billion queries to get required details from related model -- fix later
@@ -19,7 +18,18 @@ def display_production(prod_id, slug):
     credits = db.session.query(Credit).filter_by(production_id = production.production_id).all()
     notices = db.session.query(ProductionNotice, Notice, NoticeType).select_from(ProductionNotice).join(Notice).join(NoticeType).filter(ProductionNotice.production_id == production.production_id).all()
     
+    # TODO Eventually we'll need to make this dynamic 
+    poster = url_for('productions.static', filename='images/posters/poster-small.png')
+
     # Somehow this restricts valid pages to those with a valid slug
     slug = production.slug
 
-    return render_template('production.html', title = production.description ,current_user = current_user, production = production, season = season, performances = performances, credits = credits, notices = notices)
+    return render_template('production.html', title = production.description, 
+                                                poster = poster, 
+                                                sidebar = True,
+                                                current_user = current_user, 
+                                                production = production, 
+                                                season = season, 
+                                                performances = performances, 
+                                                credits = credits, 
+                                                notices = notices)
