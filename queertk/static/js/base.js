67c512b3b878a9ca02190c4e7bc51e10eb068f13
Hazel <hazel@queertk.org>
2022-04-23 00:37:28 -0400

    More work building out styles, added modal framework

--- queertk/static/js/base.js
+++ queertk/static/js/base.js
@@ -1,18 +1,7 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+var dialogEl = document.getElementById('my-dialog')
+var dialog = new A11yDialog(dialogEl)
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
+dialog.on('show', function (dialogEl, triggerEl) {
+  console.log(dialogEl)
+  console.log(triggerEl)
+})
