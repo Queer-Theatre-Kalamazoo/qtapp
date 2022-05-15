63be6bf161b0446f152a9e80ac0fde8055262ac0
Hazel James <heyzel.james@gmail.com>
2022-05-15 17:29:47 -0400

    Style updates. Fixed modal issue (cast/crew modal didn't exist on some pages so var was null). Toggle sidebar works but ugly.

--- application/static/js/base.js
+++ application/static/js/base.js
@@ -1,4 +1,6 @@
-var about_site_modal = document.getElementById('my-dialog')
+
+
+var about_site_modal = document.getElementById('about-site-dialog')
 var about_site_dialog = new A11yDialog(about_site_modal)
 
 about_site_dialog.on('show', function (about_site_modal, about_site_trigger) {
@@ -7,10 +9,19 @@ about_site_dialog.on('show', function (about_site_modal, about_site_trigger) {
 })
 
 
-var cast_crew_modal = document.getElementById('cast-crew-dialog')
-var cast_crew_dialog = new A11yDialog(cast_crew_modal)
 
-cast_crew_dialog.on('show', function (cast_crew_modal, cast_crew_trigger) {
-  console.log(cast_crew_modal)
-  console.log(cast_crew_trigger)
-})
+var cast_crew_modal = document.getElementById('cast-crew-dialog')
+if(cast_crew_modal){
+  var cast_crew_dialog = new A11yDialog(cast_crew_modal)
+
+  cast_crew_dialog.on('show', function (cast_crew_modal, cast_crew_trigger) {
+    console.log(cast_crew_modal)
+    console.log(cast_crew_trigger)
+  })
+}
+
+$(document).ready(function () {
+  $("#sidebar-toggle").on("click", function () {
+      $("aside.navigation").toggleClass("no-sidebar");
+  });
+});
