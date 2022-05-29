02a66ef42900fe8c1584840f8f5220b0aa76702c
= <=>
2022-05-28 21:09:11 -0400

    Switch to generate db connection string programatically.

--- application/database.py
+++ application/database.py
@@ -1,8 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
 from flask import current_app
 # from application import app
 
-db_conn_string = current_app.config.get('SQLALCHEMY_DATABASE_URI')
+# db_conn_string = current_app.config.get('SQLALCHEMY_DATABASE_URI')
+
+db_conn_string = URL.create(
+    drivername = 'mysql+mysqlconnector',
+    username = current_app.config.get('DATABASE_USER'),
+    password = current_app.config.get('DATABASE_PASSWORD'),
+    host = current_app.config.get('DATABASE_HOST'),
+    database = current_app.config.get('DATABASE_DB')
+)
+
 engine = create_engine(db_conn_string, future=True)
 Session = sessionmaker(engine, future=True)
--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from application import init_app
 app = init_app()
 
 if __name__ == "__main__":
-    app.run(host=0.0.0.0)
+    app.run(host="0.0.0.0")
