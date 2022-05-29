f5e6015653f00004917a50908c1a5d5f418f2cf1
= <=>
2022-05-29 06:32:54 -0400

    More responsive, style fixes.

--- application/static/js/base.js
+++ application/static/js/base.js
@@ -1,11 +1,21 @@
 /* Accordions, currently used for production notices */
-const notice_accordion = document.getElementsByClassName('prod-content-container');
-var i = 0;
-for (i=0; i<notice_accordion.length; i++) {
-  notice_accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+const notice_accordion = document.getElementsByClassName('notice-container');
+if(notice_accordion){
+  var i = 0;
+  for (i=0; i<notice_accordion.length; i++) {
+    notice_accordion[i].addEventListener('click', function () {
+      this.classList.toggle('active')
+    })
+  }
 }
+
+
+
+$(document).ready(function () {
+  $("#nav-accordion-trigger").click(function () {
+      $(".nav-accordion-body").toggle();
+  });
+});
 
 var cast_crew_modal = document.getElementById('cast-crew-dialog')
 if(cast_crew_modal){
@@ -39,3 +49,41 @@ if(ploxel_modal){
   window.options = {OID: '954681622'};
   ph.parentNode.insertBefore(s, ph);
   })();
+
+
+const nav = document.querySelector('#nav');
+const menu = document.querySelector('#menu');
+const menuToggle = document.querySelector('.nav__toggle');
+let isMenuOpen = false; // TOGGLE MENU ACTIVE STATE
+
+menuToggle.addEventListener('click', e => {
+  e.preventDefault();
+  isMenuOpen = !isMenuOpen; // toggle a11y attributes and active class
+
+  menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
+  menu.hidden = !isMenuOpen;
+  nav.classList.toggle('nav--open');
+}); // TRAP TAB INSIDE NAV WHEN OPEN
+
+nav.addEventListener('keydown', e => {
+  // abort if menu isn't open or modifier keys are pressed
+  if (!isMenuOpen || e.ctrlKey || e.metaKey || e.altKey) {
+    return;
+  } // listen for tab press and move focus
+  // if we're on either end of the navigation
+
+
+  const menuLinks = menu.querySelectorAll('.nav__link');
+
+  if (e.keyCode === 9) {
+    if (e.shiftKey) {
+      if (document.activeElement === menuLinks[0]) {
+        menuToggle.focus();
+        e.preventDefault();
+      }
+    } else if (document.activeElement === menuToggle) {
+      menuLinks[0].focus();
+      e.preventDefault();
+    }
+  }
+});
