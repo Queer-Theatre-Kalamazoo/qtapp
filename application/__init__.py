8436517a30e48c601cd3e33168e7b8db20a78065
= <=>
2022-05-30 21:54:33 -0400

    Testing

--- application/__init__.py
+++ application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_mail import Mail
 from sqlalchemy import create_engine
 from flask_ckeditor import CKEditor
-
+# Test Comment
 def init_app():
     # Initialize the core application
     app = Flask(__name__, instance_relative_config=True)
